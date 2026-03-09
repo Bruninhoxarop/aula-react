81e9f17fba9a6003bc65a84446deff9978185af4
Bruno ramoS <brunoramos1709@gmail.com>
2026-03-09 23:50:14 +0000

    mudança de estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import { logDOM } from '@testing-library/react';
 import './App.css';
 import Header from './Header';
-import Produto from './Produto'
+import Produto from './Produto';
+import {useState} from 'react';
 
 function App() {
   var x = "variável";
+  const [contador, setContador] = useState(0);
+  function increment(){
+    setContador(contador+1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Header title={x}/>
-        <img src="Octocat.png" className="App-logo" alt="logo" />
         <h1>Hello World!</h1>
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        
+        <Header title="contador" />
+        <input type="button" value="Clique" onClick={increment} /> 
+        <p>{contador} cliques!</p>
+
+        <Header title={x}/>
         <Header title="texto puro"/>
       </header>
-      <div className='div'>
+
+      <div className='desafio'>
         <h1 className='produtos'>Produtos</h1>
         <Produto nom="escova de dente"/>
         <Produto nom="pasta de dente"/>
